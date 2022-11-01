161d43dc6dc117dc1779d7224982f8d4f423b277
wenqs <wenqingshan3@163.com>
2022-11-01 21:07:33 +0800

    git 第一次提交

---
+++ src/main/java/com/wen/gitlqz/GitLqzApplication.java
@@ -0,0 +1,13 @@
+package com.wen.gitlqz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLqzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitLqzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wen/gitlqz/GitLqzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wen.gitlqz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitLqzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
